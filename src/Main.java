37c14b8a908d1587e696045ed373c832808ed898
quamilou <50031737+quamilou@users.noreply.github.com>
2020-11-10 18:06:45 +0100

    Test1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("okjfazpionfpzigfnjaoiujnfaù^p; ");
+        System.out.print("okjfazpionfpzigfnjaoiujfaù^p; ");
     }
 }
